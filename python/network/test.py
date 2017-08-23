4f659dd330027ba47d9ddfc36c0578d16af74dbf
oujezdsky <Sdg151j5jtzu84>
2017-08-23 19:25:53 -0400

    prpr

--- python/network/test.py
+++ python/network/test.py
@@ -1,27 +1,28 @@
 from socket import *
 import struct
 import binascii
-activeAps = []
-sniff = socket(AF_PACKET, SOCK_RAW, 3)
-sniff.bind(("wlan0mon", 0x0003))
 
+activeAps = []
+IFACE=""
+PROBE_RESPONSE=80
+sniff = socket(AF_PACKET, SOCK_RAW, 3)
+sniff.bind((IFACE, 0x0003))
 activeSniff = True
 while 1:
     while activeSniff:
-        fm1 = sniff.recvfrom(1024)
-        fm = fm1[0]
-        if fm[38] == "\x50":
-            probeResp = fm[38:62]
-            # print ':'.join(x.encode('hex') for x in probeResp)
-            probeHeadP = struct.unpack("!2s2s6s6s6s2s", probeResp)
-            bssidMac = binascii.hexlify(probeHeadP[4])
-            questioner = binascii.hexlify(probeHeadP[2])
+        fm = sniff.recv(2000)[38:]
+        #print ':'.join(x.encode('hex') for x in fm)
+        type,ordr=struct.unpack("!BB", fm[:2])
+        if type == PROBE_RESPONSE:
+            dur,dMac,sMac,bssidMac = struct.unpack("!H6s6s6s", fm[2:22])
+            bssidMac = binascii.hexlify(bssidMac)
+            questioner = binascii.hexlify(dMac)
             if bssidMac not in activeAps:
                 activeAps.append(bssidMac)
                 print "=============================================\n" \
-                      "SSID: %s\nBSSID mac: %s\nQuestioner mac: %s" % (fm[76:85], bssidMac, questioner)
-                if bssidMac == '3613e8404be2':
-                    activeSniff = False
+                      "SSID: %s\nBSSID mac: %s\nQuestioner mac: %s" % (fm[38:47], bssidMac, questioner)
+            if bssidMac == '3613e8404be2':
+                activeSniff = False
 
     else:
         pld = '00000c000480000002001800c0003a01ffffffffffff' + bssidMac + bssidMac + '00000700'
