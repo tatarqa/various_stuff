a3138c31ab6f7c2bc8869d8f551bb23c85b5d3b5
oujezdsky <Sdg151j5jtzu84>
2017-09-11 21:13:34 -0400

    e

--- python/drupal_finder.py
+++ python/drupal_finder.py
@@ -4,15 +4,17 @@ import cookielib
 import Queue
 from bs4 import BeautifulSoup
 from urlparse import urlparse
-#import dataset
+
+
+# import dataset
 
 
 
 def do_work(tgt):
     parsedLink = urlparse(tgt)
-    domain = parsedLink.scheme+'://'+ parsedLink.netloc
+    domain = parsedLink.scheme + '://' + parsedLink.netloc
     checked_links.append(domain)
-    dd=False
+    dd = False
     userAgent = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1')]
     browser = mechanize.Browser()
     browser.set_handle_robots(False)
@@ -24,14 +26,13 @@ def do_work(tgt):
     browser.set_cookiejar(cookie_jar)
     try:
         page = browser.open(tgt)
+        parsed_page = BeautifulSoup(page, 'html.parser')
     except:
         return
     if 'Sun, 19 Nov 1978 05:00:00 GMT' in page._headers.dict.viewvalues():
         if domain not in drupal_sites:
             drupal_sites.append(domain)
-            dd=True
-
-    parsed_page = BeautifulSoup(page, 'html.parser')
+            dd = True
     for anchor in parsed_page.find_all('a'):
         startLink = anchor.get('href')
         if startLink:
@@ -39,13 +40,13 @@ def do_work(tgt):
                 continue
             else:
                 parsedLink = urlparse(startLink)
-                new_link_domain = parsedLink.scheme+'://'+ parsedLink.netloc
+                new_link_domain = parsedLink.scheme + '://' + parsedLink.netloc
                 if not new_link_domain in checked_links and new_link_domain.endswith('.cz'):
                     links.append(new_link_domain)
                     domains.append(new_link_domain)
 
     if dd:
-        print domain+', JE DRUPAL'
+        print domain + ', JE DRUPAL'
 
 
 def worker():
@@ -57,14 +58,16 @@ def worker():
 
         else:
             q.task_done()
+
+
 q = Queue.Queue()
 links = []
 domains = []
 drupal_sites = []
 checked_links = []
 for i in range(666):
-    #db = dataset.connect('sqlite:///domeny.db')
-    #domains_table = db['all_domains']
+    # db = dataset.connect('sqlite:///domeny.db')
+    # domains_table = db['all_domains']
     t = threading.Thread(target=worker, args=())
     t.daemon = True
     t.start()
@@ -84,7 +87,6 @@ while 1:
 print "\n[-] enter domain name e.g. %sgoogle.com" % prefx
 
 links.append(prefx + raw_input('>' + prefx + ""))
-
 
 while 1:
     for item in links:
