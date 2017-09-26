4b4acdab95d3c0388db1e34d444f0a328c7fe7e7
oujezdsky <Sdg151j5jtzu84>
2017-09-26 10:31:54 -0400

    e

---
+++ python/project_euler/Problem_7.py
@@ -0,0 +1,21 @@
+primeNumbers = []
+
+
+def sqrroot():
+    yield 2
+    for i in range(1000000):
+        isPrime = ''
+        if not i % 2 == 0 and i > 1:
+            for nr in primeNumbers:
+                if nr < i / 2:
+                    if i % nr == 0 and i not in primeNumbers:
+                        isPrime = False
+            if i not in primeNumbers and isPrime is not False:
+                primeNumbers.append(i)
+                yield i
+
+
+for i, number in enumerate(sqrroot(), start=1):
+    print i, '[]', number
+    if i == 10001:
+        break
