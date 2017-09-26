a081220c36263ff163465f06ac9fa3c31d409804
oujezdsky <Sdg151j5jtzu84>
2017-09-26 09:53:40 -0400

    e

--- python/project_euler/Problem_5.py
+++ python/project_euler/Problem_5.py
@@ -11,16 +11,16 @@ def main():
     a = 1
     while a:
         for item in sourcelist:
-            a = isok(item)
+            a = isok_provider(item)
             print item
 
 
-def isok(nr):
+def isok_provider(nr):
     for n in range(1, 21):
-        checkRange(nr/n)
+        isok(nr/n)
 
 
-def checkRange(nr):
+def isok(nr):
     for n in range(1, 21):
         if nr % n != 0:
             return
