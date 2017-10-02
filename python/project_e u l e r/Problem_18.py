59c258bdb1f4d087e2a72be5475b8678b61da647
oujezdsky <Sdg151j5jtzu84>
2017-10-02 10:59:05 -0400

    problem 18 v.2

--- python/project_e u l e r/Problem_18.py
+++ python/project_e u l e r/Problem_18.py
@@ -1,5 +1,5 @@
 def load_input(data_list):
-    with open("Problem_67_input.txt", 'r') as input_file:
+    with open("Problem_18_input.txt", 'r') as input_file:
         for line in reversed(input_file.readlines()):
             text_block = ''
             row = []
@@ -16,16 +16,16 @@ def load_input(data_list):
 def get_n_highest_numbers(numbers):
     row_len = len(numbers)
     if row_len > 4:
-        highest_numbers = sorted(numbers, key=int, reverse=True)[:row_len / 4 + 2]
+        highest_numbers = sorted(numbers, key=int, reverse=True)[:row_len / 4 + 3]
     else:
         highest_numbers = numbers
     yield highest_numbers
 
 
-def analyze_input():
+def analyze_input(start_routes):
     data_list = []
     load_input(data_list)
-    final_route = ()
+    final_routes = ()
     for data_row in data_list:
         found = False
         d = 0
@@ -34,22 +34,42 @@ def analyze_input():
         while found != True:
             number = data_row[d]
             if number in highest_numbers:
-                if final_route is not ():
-                    if final_route[1] < d:
-                        return False
+                if final_routes is not ():
+                    if final_routes[1] < d:
+                        print 'FAIL'
+                        return (False)
                         # TODO repeat the script using different values
-                    if final_route[1] in range(d - 1, d):
-                        final_route = (number, d)
+
+                    if final_routes[1] in range(d - 1, d):
+                        final_routes = (number, d)
+                        print number
                         found = True
                 else:
-                    final_route = (number, d)
-                    found = True
+                    if start_routes != ():
+                        if (number, d) not in start_routes:
+                            final_routes = (number, d)
+                            start_routes.append(final_routes)
+                            print number
+                            found = True
+                    else:
+                        final_routes = (number, d)
+                        start_routes.append(final_routes)
+                        print number
+                        found = True
+            if d == len(data_list):
+                return True
             d += 1
 
 
 def main():
-    if analyze_input() == False:
-        print 'It does not work'
+    not_finished = True
+    first_routes = []
+    first_result = analyze_input(first_routes)
+    while not_finished is True:
+        if first_result == False:
+            first_result = analyze_input(first_routes)
+        else:
+            not_finished = False
 
 
 if __name__ == "__main__":
