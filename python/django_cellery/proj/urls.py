9c8decc33cdb5ade54aa29691b67a9dd6a1184b7
jiri oujezdsky <jiri.oujezdsky@nordictelecom.cz>
2018-02-19 14:51:57 +0100

    cellery django

---
+++ python/django_cellery/proj/urls.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.urls import path
+
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
