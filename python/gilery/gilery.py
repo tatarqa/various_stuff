47841a200331cfccc90192308bfcf31fc902c844
jiri oujezdsky <jiri.oujezdsky@nordictelecom.cz>
2018-03-27 15:01:47 +0200

    wtf is asyncio anyway

--- python/gilery/gilery.py
+++ python/gilery/gilery.py
@@ -1,4 +1,3 @@
-import sys
 import time
 import asyncio
 import aiohttp
@@ -7,8 +6,17 @@ import os
 
 #urls = [f'https://wikileaks.org/clinton-emails/emailid/{i}' for i in range(1,99999)]
 urls = [
-    f'https://wikileaks.org/clinton-emails/emailid/{i}' for i in range(2, 50)]
+    f'https://wikileaks.org/clinton-emails/emailid/{i}' for i in range(1, 5)]
 
+import random
+import time
+async def paprik():
+    time.sleep(6)
+    while 1: 
+        print ('hwmsnhhh')
+        if random.randint(1,5)==2: break
+ppp=asyncio.gather(paprik())
+print (666)
 
 async def fetch(url, loop):
     async with aiohttp.ClientSession(loop=loop) as session:
@@ -16,11 +24,11 @@ async def fetch(url, loop):
             async with session.get(url, allow_redirects=False, timeout=10) as response:
                 if response.status == 200:
                     responseDocument = await response.read()
-                    return (url, responseDocument.decode('utf-8'))
+                    return responseDocument.decode('utf-8')
                 else:
-                    return (url, -1)
+                    return -1
         except Exception:
-            return (url, -1)
+            return -1
 
 
 async def main(loop):
@@ -34,7 +42,7 @@ async def main(loop):
     await asyncio.gather(*tasks)
     results = map(lambda x: x.result(), tasks)
     for i, fetch_result in enumerate(results):
-        (url, responseDocument) = fetch_result
+        responseDocument = fetch_result
         with open(os.path.join(outdir, f'gilery{i}.html'), 'w') as htmlOutput:
             htmlOutput.write(responseDocument)
 
