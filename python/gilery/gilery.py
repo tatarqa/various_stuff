7bfac33ad7d2b0056489cf9c24f91a2a25539da6
jiri oujezdsky <jiri.oujezdsky@nordictelecom.cz>
2018-03-27 13:19:17 +0200

    e

--- python/gilery/gilery.py
+++ python/gilery/gilery.py
@@ -2,21 +2,23 @@ import sys
 import time
 import asyncio
 import aiohttp
+import os
 
 
+#urls = [f'https://wikileaks.org/clinton-emails/emailid/{i}' for i in range(1,99999)]
+urls = [
+    f'https://wikileaks.org/clinton-emails/emailid/{i}' for i in range(2, 50)]
 
-urls = [f'https://wikileaks.org/clinton-emails/emailid/{i}' for i in range(1,99999)]
 
 async def fetch(url, loop):
     async with aiohttp.ClientSession(loop=loop) as session:
         try:
-            start_time = time.perf_counter()
             async with session.get(url, allow_redirects=False, timeout=10) as response:
-                end_time = time.perf_counter()
                 if response.status == 200:
                         # TOU DOU
-                    duration = (end_time - start_time) * 1000
-                    return (url, duration)
+                    #duration = (end_time - start_time) * 1000
+                    responseDocument = await response.read()
+                    return (url, responseDocument.decode('utf-8'))
                 else:
                     return (url, -1)
         except Exception:
@@ -25,13 +27,18 @@ async def fetch(url, loop):
 
 async def main(loop):
     tasks = []
+    outdir = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'htmlout')
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
     for url in urls:
         tasks.append(asyncio.ensure_future(fetch(url, loop)))
     await asyncio.gather(*tasks)
     results = map(lambda x: x.result(), tasks)
-    for fetch_result in results:
-        (url, result) = fetch_result
-        print("{} ".format(url) + "{}ms".format(result))
+    for i, fetch_result in enumerate(results):
+        (url, responseDocument) = fetch_result
+        with open(os.path.join(outdir, f'gilery{i}.html'), 'w') as htmlOutput:
+            htmlOutput.write(responseDocument)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main(loop))
